e3f15bd7aec39f55a292b9eaf5f727e2ad4b4c84
JunYoungKr <kop981020@naver.com>
2023-11-10 01:47:28 +0900

    [add]마이페이지 디자인 완성

--- src/Components/Login/MyPage/MyPage.js
+++ src/Components/Login/MyPage/MyPage.js
@@ -18,46 +18,54 @@ const MyPage = () => {
   };
 
   const userId = localStorage.getItem("userId");
+  const birth = localStorage.getItem("birth");
+  const gender = localStorage.getItem("gender");
+  const kakaoId = localStorage.getItem("kakaoId");
+  const major = localStorage.getItem("major");
+  const name = localStorage.getItem("name");
+  const phoneNum = localStorage.getItem("phoneNum");
+  const sejongEmail = localStorage.getItem("sejongEmail");
+  const studentId = localStorage.getItem("studentId");
+
   return (
     <>
       <h2 className={styles.myInfo}>내 정보</h2>
       <div className={styles.logoutContainer}>
         <div className={styles.div1}>
           <p className={styles.leftFont}>아이디</p>
-          <p className={styles.rightFont}>123</p>
-          {/* <p className={styles.rightFont}>{userId}</p> */}
+          <p className={styles.rightFont}>{userId}</p>
         </div>
         <div className={styles.div1}>
           <p className={styles.leftFont}>이메일</p>
-          {/* <p className={styles.rightFont}>{sejongEmail}</p> */}
+          <p className={styles.rightFont}>{sejongEmail}</p>
         </div>
         <div className={styles.div1}>
           <p className={styles.leftFont}>카카오톡 아이디</p>
-          {/* <p className={styles.rightFont}>{kakaoId}</p> */}
+          <p className={styles.rightFont}>{kakaoId}</p>
         </div>
         <div className={styles.div1}>
           <p className={styles.leftFont}>전화번호</p>
-          {/* <p className={styles.rightFont}>{phoneNum}</p> */}
+          <p className={styles.rightFont}>{phoneNum}</p>
         </div>
         <div className={styles.div1}>
           <p className={styles.leftFont}>이름</p>
-          {/* <p className={styles.rightFont}>{name}</p> */}
+          <p className={styles.rightFont}>{name}</p>
         </div>
         <div className={styles.div1}>
           <p className={styles.leftFont}>생년월일</p>
-          {/* <p className={styles.rightFont}>{birth}</p> */}
+          <p className={styles.rightFont}>{birth}</p>
         </div>
         <div className={styles.div1}>
           <p className={styles.leftFont}>성별</p>
-          {/* <p className={styles.rightFont}>{gender}</p> */}
+          <p className={styles.rightFont}>{gender}</p>
         </div>
         <div className={styles.div1}>
           <p className={styles.leftFont}>단과대</p>
-          {/* <p className={styles.rightFont}>{major}</p> */}
+          <p className={styles.rightFont}>{major}</p>
         </div>
         <div className={styles.div1}>
           <p className={styles.leftFont}>학번</p>
-          {/* <p className={styles.rightFont}>{studentId}</p> */}
+          <p className={styles.rightFont}>{studentId} 학번</p>
         </div>
         <button className={styles.logoutButton} onClick={handleLogout}>
           <p className={styles.logoutFont}>로그아웃</p>
