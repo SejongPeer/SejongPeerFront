076b290d113397f62c2180e2ac18afe40d5a1fcb
JunYoungKr <kop981020@naver.com>
2023-11-09 21:36:05 +0900

    [add]마이페이지 디자인 및 로그아웃 테스트

--- src/Components/Login/MyPage/MyPage.js
+++ src/Components/Login/MyPage/MyPage.js
@@ -1,31 +1,65 @@
+import { useNavigate } from "react-router-dom";
 import styles from "../MyPage/MyPage.module.css";
 
 const MyPage = () => {
-  const test = () => {
-    return console.log("로그아웃 버튼입니다!");
+  const navigate = useNavigate();
+  // const test = () => {
+  //   return console.log("로그아웃 버튼입니다!");
+  // };
+
+  const handleLogout = () => {
+    deleteCookie("session"); // 여기에 삭제할 쿠키의 이름을 넣는다
+    alert("로그아웃 되었습니다!");
+    navigate("/login");
   };
+
+  const userId = localStorage.getItem("userId");
   return (
-    <div className={styles.logoutContainer}>
-      <div className={styles.div1}>
-        <p>아이디</p>
-        <p>sejonge</p>
+    <>
+      <h2 className={styles.myInfo}>내 정보</h2>
+      <div className={styles.logoutContainer}>
+        <div className={styles.div1}>
+          <p className={styles.leftFont}>아이디</p>
+          <p className={styles.rightFont}>123</p>
+          {/* <p className={styles.rightFont}>{userId}</p> */}
+        </div>
+        <div className={styles.div1}>
+          <p className={styles.leftFont}>이메일</p>
+          {/* <p className={styles.rightFont}>{sejongEmail}</p> */}
+        </div>
+        <div className={styles.div1}>
+          <p className={styles.leftFont}>카카오톡 아이디</p>
+          {/* <p className={styles.rightFont}>{kakaoId}</p> */}
+        </div>
+        <div className={styles.div1}>
+          <p className={styles.leftFont}>전화번호</p>
+          {/* <p className={styles.rightFont}>{phoneNum}</p> */}
+        </div>
+        <div className={styles.div1}>
+          <p className={styles.leftFont}>이름</p>
+          {/* <p className={styles.rightFont}>{name}</p> */}
+        </div>
+        <div className={styles.div1}>
+          <p className={styles.leftFont}>생년월일</p>
+          {/* <p className={styles.rightFont}>{birth}</p> */}
+        </div>
+        <div className={styles.div1}>
+          <p className={styles.leftFont}>성별</p>
+          {/* <p className={styles.rightFont}>{gender}</p> */}
+        </div>
+        <div className={styles.div1}>
+          <p className={styles.leftFont}>단과대</p>
+          {/* <p className={styles.rightFont}>{major}</p> */}
+        </div>
+        <div className={styles.div1}>
+          <p className={styles.leftFont}>학번</p>
+          {/* <p className={styles.rightFont}>{studentId}</p> */}
+        </div>
+        <button className={styles.logoutButton} onClick={handleLogout}>
+          <p className={styles.logoutFont}>로그아웃</p>
+        </button>
       </div>
-      <div className={styles.div1}>
-        <p>이메일</p>
-        <p>sejongpeer@sejong.ac.kr</p>
-      </div>
-      <div className={styles.div1}>
-        <p>카카오톡 아이디</p>
-        <p>sejongsejong</p>
-      </div>
-      <div className={styles.div1}>
-        <p>전화번호</p>
-        <p>01012345678</p>
-      </div>
-      <button className={styles.logoutButton} onClick={test}>
-        로그아웃
-      </button>
-    </div>
+    </>
   );
 };
 
