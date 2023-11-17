58944da39ca224decf5b90fd14999fbd7b26ae1e
AhnRian0 <00rian@naver.com>
2023-11-17 14:12:39 +0900

    [add]혼밥매칭 버튼(이전,다음) 수정,혼밥 /matching 페이지 이미지 추가

--- src/Components/Honbob/HonbobWaiting/HonbobWaiting.js
+++ src/Components/Honbob/HonbobWaiting/HonbobWaiting.js
@@ -1,9 +1,19 @@
 import style from "./HonbobWaiting.module.css";
-
+import hohbobWaitingLogo from "../../../Assets/honbobWaitingLogo.png";
+import { useNavigate } from "react-router-dom";
 const HonbobWaiting = () => {
+
+  const navigate=useNavigate();
+  const moveToMain=()=>{
+    navigate("/main");
+  }
   return (
     <div clsaaName={style.container}>
       <div className={style.TextBox}>
+        <img
+          src={hohbobWaitingLogo}
+          className={style.honbobWaitingImg}
+        />
         <p className={style.Text1}>밥짝꿍 찾는 중...</p>
         <p className={style.Text2}>
           밥짝꿍 매칭 시 메시지로
@@ -13,7 +23,7 @@ const HonbobWaiting = () => {
       </div>
 
       <div className={style.BtnBox}>
-        <button className={style.moveToHomeBtn}>홈페이지로 이동</button>
+        <button className={style.moveToHomeBtn} onClick={moveToMain}>홈페이지로 이동</button>
         <button className={style.cancelBtn}>혼밥탈출 취소</button>
       </div>
     </div>
