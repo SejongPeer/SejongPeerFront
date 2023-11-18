eeaa8c7d073e54c9858434082dd3c11fe253168e
JunYoungKr <kop981020@naver.com>
2023-11-18 14:48:12 +0900

    feat: 회원가입 학년 추가

--- src/Components/Login/SignUp/SignUpElement.js
+++ src/Components/Login/SignUp/SignUpElement.js
@@ -1,70 +1,72 @@
-import { useEffect, useState } from 'react';
-import InputTextBox from './InputTextBox';
+import { useEffect, useState } from "react";
+import InputTextBox from "./InputTextBox";
 
-import style from './SignUpElement.module.css';
-import EmailBox from './EmailBox';
-import GenderSignUp from './GenderSignUp';
-import MajorSignUp from './MajorSignUp';
+import style from "./SignUpElement.module.css";
+import EmailBox from "./EmailBox";
+import GenderSignUp from "./GenderSignUp";
+import MajorSignUp from "./MajorSignUp";
 
 const SignUpElement = (props) => {
-    const [isEmail, setIsEmail] = useState(false);
-    const [isError, setIsError] = useState('');
-    const isGender = props.id === "gender";
-    const isMajor = props.id === "major";
+  const [isEmail, setIsEmail] = useState(false);
+  const [isError, setIsError] = useState("");
+  const isGender = props.id === "gender";
+  const isMajor = props.id === "major";
 
-    const errorHandler = (error) => {
-        setIsError(error);
-        props.signUpErrorHandler(error);
-    };
+  const errorHandler = (error) => {
+    setIsError(error);
+    props.signUpErrorHandler(error);
+  };
 
-    useEffect(() => {
-        if(props.id === "email"){
-            setIsEmail(true);
-        } else {
-            setIsEmail(false);
-        }
-    }, [props.id]);
-    
-    return <div className={style.container}>
-        {props.id !== "check" &&<p className={style.title}>{props.title}</p>}
+  useEffect(() => {
+    if (props.id === "email") {
+      setIsEmail(true);
+    } else {
+      setIsEmail(false);
+    }
+  }, [props.id]);
 
-        {isEmail ? (
-            <EmailBox
-                id={props.id}
-                name={props.name}
-                emailData={props.emailData}
-                errorHandler={errorHandler}
-            />
-        ) : isGender ? (
-            <GenderSignUp 
-                id={props.id}
-                name={props.name}
-                genderData={props.genderData}
-            />
-        ) : isMajor? (
-            <MajorSignUp 
-                id={props.id}
-                name={props.name}
-                majorData={props.majorData}
-            />
-        ) : (
-            <InputTextBox 
-                id={props.id}
-                name={props.name}
-                errorHandler={errorHandler}
-                
-                idData={props.idData}
-                pwdData={props.pwdData}
-                nameData={props.nameData}
-                birthData={props.birthData}
-                kakaoData={props.kakaoData}
-                phoneNumData={props.phoneNumData}
-                studentNumData={props.studentNumData}
-            />
-        )}
+  return (
+    <div className={style.container}>
+      {props.id !== "check" && <p className={style.title}>{props.title}</p>}
 
-        {isError && (<p className={style.error}>{isError}</p>)}
-    </div>;
+      {isEmail ? (
+        <EmailBox
+          id={props.id}
+          name={props.name}
+          emailData={props.emailData}
+          errorHandler={errorHandler}
+        />
+      ) : isGender ? (
+        <GenderSignUp
+          id={props.id}
+          name={props.name}
+          genderData={props.genderData}
+        />
+      ) : isMajor ? (
+        <MajorSignUp
+          id={props.id}
+          name={props.name}
+          majorData={props.majorData}
+        />
+      ) : (
+        <InputTextBox
+          id={props.id}
+          name={props.name}
+          errorHandler={errorHandler}
+          idData={props.idData}
+          pwdData={props.pwdData}
+          nameData={props.nameData}
+          birthData={props.birthData}
+          kakaoData={props.kakaoData}
+          phoneNumData={props.phoneNumData}
+          studentNumData={props.studentNumData}
+          gradeData={props.gradeData}
+        />
+      )}
+
+      {isError && <p className={style.error}>{isError}</p>}
+    </div>
+  );
 };
 
 export default SignUpElement;
