daaa7216ac2f532ad8ec1cbb525f246c1a2ea73a
Youjiiin <yujinjang24@gmail.com>
2023-11-20 02:38:50 +0900

    feat: 버디 매칭 완료시 중복신청 불가하도록, 버디 매칭 취소

--- src/Components/Buddy/BuddyWaiting/BuddyWaiting.js
+++ src/Components/Buddy/BuddyWaiting/BuddyWaiting.js
@@ -7,8 +7,36 @@ const BuddyWaiting = ()=>{
     const moveToMain=()=>{
         navigate("/main");
     }
+    const cancelBuddy = async() => {
+        try {
+            const response = await fetch(
+              process.env.REACT_APP_BACK_SERVER + "/buddy/cancel",
+              {
+                method: "POST",
+                body: JSON.stringify(),
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              }
+            );
+      
+            const data = await response.json(); // data 변수를 await로 초기화
+            console.log(data.message);
+      
+            if (!response.ok) {
+              throw new Error(data.message);
+            }
+      
+            alert(data.message);
+            console.log(data.message);
+            navigate("/main");
+          } catch (error) {
+            console.error(error.message);
+            alert(error.message);
+          }
+    };
     return(
-        <div clsaaName={style.container}>
+        <div className={style.container}>
             <div className={style.TextBox}>
             <img src={buddyTextRed} className={style.buddyLogo}/>
             <p className={style.Text1}>216명의 학생들이 버디를 찾고 있어요!</p>
@@ -20,7 +48,7 @@ const BuddyWaiting = ()=>{
     
             <p className={style.Text3}>버디 매칭은 최대 4일 정도 소요됩니다.</p>
             <button className={style.moveToHomeBtn} onClick={moveToMain}>메인 페이지로 이동</button>
-            <button className={style.cancelBtn}>버디 신청 취소</button>
+            <button className={style.cancelBtn} onClick={cancelBuddy}>버디 신청 취소</button>
             </div>
         </div>
     )
