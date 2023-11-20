356801c4f4f8654656b6c4185ed33d54b7c33b4f
Youjiiin <yujinjang24@gmail.com>
2023-11-20 14:25:14 +0900

    style: css 수정

--- src/Components/Buddy/BuddyStart/BuddyStart2.js
+++ src/Components/Buddy/BuddyStart/BuddyStart2.js
@@ -28,10 +28,12 @@ const BuddyStart2 = () => {
                 alert("버디를 찾는중인 사용자입니다.");
                 navigate("/buddy/waiting");
             } else if (response.status === 200) {
-                dispatch(fetchData("Buddy", navigate));
+                navigate("/buddy/matching");
+            } else {
+                alert("로그인이 필요한 서비스입니다!")
             }
-
         } catch (error) {
+            alert("로그인이 필요한 서비스입니다!");
             console.log(error.message);
             alert(error.message);
         }
@@ -45,8 +47,8 @@ const BuddyStart2 = () => {
     const BackHandler = () => {
         navigate('/buddy/start1');
     };
-    const txt1 = "한명의 학우와 한 학기 동안 버디가 되며, 다음 학기에 새로운 버디를 찾을 수 있습니다.";
-    const duration = "매칭은 부스 운영중에만 진행됩니다.";
+    const txt1 = "한명의 학우와 한 학기 동안 버디가 되며,\n 다음 학기에 새로운 버디를 찾을 수 있습니다.";
+    const duration = "매칭은 컨퍼런스 기간\n (11월 20일~11월 23일)에만 진행됩니다.";
 
     return <div className={style.container}>
         <div className={style.top}>
@@ -59,7 +61,7 @@ const BuddyStart2 = () => {
             <div className={style.wrapper}>
                 <p className={style.text2}>버디(Buddy)는 <span className={style.highlight}>‘동료'</span>이자 <span className={style.highlight}>‘단짝'</span>이라는 의미로, 학교생활을 함께 할 <span className={style.highlight}>짝꿍을 찾는 서비스</span>입니다.</p>
                 <p className={style.text2}>{txt1}</p>
-                <p className={style.text2}>{duration}</p>
+                <p className={style.text_red}>{duration}</p>
             </div>
             
         </div>
