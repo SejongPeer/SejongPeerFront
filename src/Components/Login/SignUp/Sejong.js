89c6cb41e10fac3c35e3dcf6f8d1f2f24486b6ed
Youjiiin <yujinjang24@gmail.com>
2024-02-06 01:08:56 +0900

    feat: 세종어스 통신

--- src/Components/Login/SignUp/Sejong.js
+++ src/Components/Login/SignUp/Sejong.js
@@ -1,43 +1,50 @@
 import { useState } from "react";
-import style from "../SignIn/SignIn.module.css"
-import SignInBox from "../SignIn/SignInBox"
-import axios from "axios"
+import style from "../SignIn/SignIn.module.css";
+import SignInBox from "../SignIn/SignInBox";
+import axios from "axios";
 
 const Sejong = () => {
-    const [id, setId] = useState("");
-    const [pwd, setPWd] = useState("");
+  const [id, setId] = useState("");
+  const [pwd, setPWd] = useState("");
 
-    const inputID = (idinput) => {
-        setId(idinput);
-    };
-    const inputPwd = (pwdinput) => {
-        setPWd(pwdinput);
-    };
+  const inputID = (idinput) => {
+    setId(idinput);
+  };
+  const inputPwd = (pwdinput) => {
+    setPWd(pwdinput);
+  };
 
-    const isSejong = () => {
-        axios.post('https://auth.imsejong.com/auth?method=ClassicSession HTTP/1.1', {
-            "id": "19010656",
-            "pw": "wkddbwls0204"
-        })
-        .then((response) => console.log(response.data))
-        .catch((err) => {
-            console.log(err.message)
-        })
-    }
-    
-    return <div className={style.container}>
-        <div className={style.explain_box}>
-            <p className={style.explain_title}>세종대학교 학생 인증</p>
-            <p className={style.explain}>세종대학교 통합 로그인을 통해 인증합니다.</p>
-            <p className={style.explain}>(세종대학교 포털 ID/PW)</p>
-        </div>
-        <SignInBox inputID={inputID} name="포털 로그인 아이디(학번)" />
-        <SignInBox inputPwd={inputPwd} name="포털 로그인 비밀번호" id="pwd" />
-        <button className={style.signInBtn} onClick={isSejong}>
+  const isSejong = () => {
+    console.log(id);
+    console.log(pwd);
+    axios
+      .post("/api?method=PortalSSOToken", {
+        id: id,
+        pw: pwd,
+      })
+      .then((response) => console.log(response.data))
+      .catch((err) => console.log(err.message));
+  };
+
+  return (
+    <div className={style.container}>
+      <div className={style.explain_box}>
+        <p className={style.explain_title}>세종대학교 학생 인증</p>
+        <p className={style.explain}>
+          세종대학교 통합 로그인을 통해 인증합니다.
+        </p>
+        <p className={style.explain}>(세종대학교 포털 ID/PW)</p>
+      </div>
+      <SignInBox inputID={inputID} name="포털 로그인 아이디(학번)" />
+      <SignInBox inputPwd={inputPwd} name="포털 로그인 비밀번호" id="pwd" />
+      <button className={style.signInBtn} onClick={isSejong}>
         통합 로그인 인증
       </button>
-      <a href="http://portal.sejong.ac.kr/" className={style.sejong}>http://portal.sejong.ac.kr/</a>
+      <a href="http://portal.sejong.ac.kr/" className={style.sejong}>
+        http://portal.sejong.ac.kr/
+      </a>
     </div>
-}
+  );
+};
 
-export default Sejong
+export default Sejong;
