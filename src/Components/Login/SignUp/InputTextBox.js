f7ae472221e6263317e334f8f3afe5dfe8fe866d
김주영 <kimjuyoung@gimjuyeong-ui-noteubug.local>
2024-02-19 05:32:00 +0900

    회원가입 코드 수정2 (id 랑 학년 데이터 빼고 데이터 받아와 지 는 같)

--- src/Components/Login/SignUp/InputTextBox.js
+++ src/Components/Login/SignUp/InputTextBox.js
@@ -3,36 +3,31 @@ import style from "./InputTextBox.module.css";
 
 const InputTextBox = (props) => {
   const [inputNum, setInputNum] = useState(false);
-  const [birthNum, setBirthNum] = useState("");
-  const isPWD = props.id === "pwd";
+  //const [birthNum, setBirthNum] = useState("");
+  const isPWD = props.id === "pwd" && "pwdCheck";
 
   function handleKeyPress(event) {
     const charCode = event.which ? event.which : event.keyCode;
     if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-      event.preventDefault();
+      event.preventDefault(); 
     }
   }
 
   const inputHandler = (event) => {
     let inputValue = event.target.value;
-
-    //ID
+    //아이디
     if (props.id === "userId") {
       props.idData(inputValue);
-
       if (inputValue.length >= 8) {
         props.errorHandler("");
       } else {
         props.errorHandler("* 아이디는 8자 이상 작성해주세요");
       }
-
-      //PWD
+    //패스워드 
     } else if (props.id === "pwd") {
       props.pwdData(inputValue);
-
       // 영어와 숫자를 모두 포함하는지 확인하는 정규 표현식
       const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/;
-
       if (inputValue.length >= 10) {
         if (regex.test(inputValue)) {
           props.errorHandler("");
@@ -44,36 +39,27 @@ const InputTextBox = (props) => {
       } else {
         props.errorHandler("* 비밀번호는 10자이상으로 작성해주세요");
       }
-
-      //Name
-    } else if (props.id === "name") {
-      props.nameData(inputValue);
-
-      //Kakaoid
-    } else if (props.id === "kakaoid") {
-      props.kakaoData(inputValue);
-
-      //PhoneNum
-    } else if (props.id === "phoneNum") {
-      props.phoneNumData(inputValue);
-
-      if (inputValue.includes("-")) {
-        props.errorHandler("* 하이픈(-)은 빼고 작성해주세요");
-      } else {
-        if (inputValue.length < 11) {
-          props.errorHandler("* 전화번호는 11자리로 입력해주세요");
-        } else if (inputValue.length === 11) {
+    } else if (props.id === "pwdCheck") {
+      props.pwdData(inputValue);
+      // 영어와 숫자를 모두 포함하는지 확인하는 정규 표현식
+      const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/;
+      if (inputValue.length >= 10) {
+        if (regex.test(inputValue)) {
           props.errorHandler("");
-        } else if (inputValue.length > 11) {
-          props.errorHandler("* 전화번호는 11자리로 입력해주세요");
-          event.preventDefault();
+        } else {
+          props.errorHandler(
+            "* 비밀번호는 영어와 숫자를 모두 포함해야 합니다."
+          );
         }
+      } else {
+        props.errorHandler("* 비밀번호는 10자이상으로 작성해주세요");
       }
-
-      //StudentNum
+      props.pwdCheckData(inputValue);
+    }//이름
+    else if (props.id === "name") {
+      props.nameData(inputValue);
     } else if (props.id === "studentNum") {
       props.studentNumData(inputValue);
-
       if (inputValue.length < 2) {
         props.errorHandler("* 학번은 2자로 작성해주세요");
       } else if (inputValue.length === 2) {
@@ -89,15 +75,55 @@ const InputTextBox = (props) => {
         props.errorHandler("");
       } else {
         props.errorHandler("* 학번은 1자로 작성해주세요");
-      }
+      }    
+    }//학년 
+    else if (props.id === "grade") {
+      props.gradeData(inputValue);
+    }//닉네임 
+    else if (props.id === "nickname") {
+      props.nickNameData(inputValue);
+    }//카카오아이디 
+    else if (props.id === "kakaoid") {
+      props.kakaoData(inputValue);
+    }//전화번호 
+    else if (props.id === "phoneNum") {
+      props.phoneNumData(inputValue);
+      if (inputValue.includes("-")) {
+        props.errorHandler("* 하이픈(-)은 빼고 작성해주세요");
+      } else {
+        if (inputValue.length < 11) {
+          props.errorHandler("* 전화번호는 11자리로 입력해주세요");
+        } else if (inputValue.length === 11) {
+          props.errorHandler("");
+        } else if (inputValue.length > 11) {
+          props.errorHandler("* 전화번호는 11자리로 입력해주세요");
+          event.preventDefault();
+        }
+      }    } else if (props.id === "gender") {
+      props.genderData(inputValue);
+    }//학부 
+    else if (props.id === "major") {
+      props.majorData(inputValue);
+    }//전공 
+    else if (props.id === "college") {
+      props.collegeData(inputValue);
+    }//(복수전공)학부 
+    else if (props.id === "double_major") {
+      props.doublemajorData(inputValue);
+    }//(복수전공)전공 
+    else if (props.id === "double_college") {
+      props.doubleCollegeData(inputValue);
+    } 
+    else {
+      console.warn("Unhandled input type: ", props.id);
     }
   };
 
-  useEffect(() => {
+  useEffect(() => {//숫자
     if (
-      props.id === "birth" ||
       props.id === "phoneNum" ||
-      props.id === "studentNum"
+      props.id === "studentNum" ||
+      props.id === "grade"
     ) {
       setInputNum(true);
     } else {
@@ -111,10 +137,10 @@ const InputTextBox = (props) => {
         <input
           className={style.inputText}
           placeholder={props.name}
-          onKeyDown={handleKeyPress}
+          onKeyDown={handleKeyPress}//숫자가 아닌 입력 방지
           onChange={inputHandler}
         />
-      ) : isPWD ? (
+      ) : isPWD ? (//패스워드입력
         <input
           className={style.inputText}
           type="password"
