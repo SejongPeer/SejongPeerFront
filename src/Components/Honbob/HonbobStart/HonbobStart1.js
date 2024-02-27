ecaa79859d98413c4fa5f317a56fb5bf4c9def99
AhnRian0 <00rian@naver.com>
2024-02-28 04:13:43 +0900

    혼밥 페이지 Css 1차 완성

--- src/Components/Honbob/HonbobStart/HonbobStart1.js
+++ src/Components/Honbob/HonbobStart/HonbobStart1.js
@@ -14,27 +14,24 @@ const HonbobStart1 = () => {
   };
 
   return (
-    <div className={style.container}>
-      <div className={style.top2}>
-        <img
-          className={style.right}
-          src={sejongHonbobcat}
-          alt="sejongHonbobcat"
-          style={{ width: "32vh", height: "32vh" }}
-        />
-        <p className={style.text1}>혼밥하기 싫을땐?</p>
-        <img
-          className={style.honbobText}
-          src={honbobEscape}
-          alt="honbobEscape"
-        />
-      </div>
+    <div className={style.Container}>
+      <div className={style.InnerContainer}>
+        <div className={style.top}>
+          <button className={style.informBtn}>이용방법 확인 <div className={style.informImg}></div></button>
 
-      <div className={style.bottom}>
-        <p className={style.text}>오늘은 48명의 밥짝꿍들이 매칭됐어요!XD </p>
-        <button onClick={HonbobHandler} className={style.btn1}>
-          밥짝꿍 찾기
-        </button>
+        </div>
+        <div className={style.middle}>
+          <div className={style.honbobLogo}></div>
+          <div className={style.informContext}>
+            <div className={style.context1}>혼밥탈출은 혼자 밥을 먹어야 하는</div>
+            <div className={style.context1}>상황에 <span style={{ color: "red" }}>밥짝꿍</span>을 찾는 서비스입니다.</div>
+          </div>
+          <div className={style.findContext}>
+            <div className={style.raccoon}></div> <div><span style={{ fontWeight: "700" }}>216</span>명의 학생들이 밥짝꿍을 찾고있어요!</div>
+          </div>
+          <button className={style.submitBtn} onClick={HonbobHandler}>혼밥탈출 신청하기</button>
+        </div>
+
       </div>
     </div>
   );
