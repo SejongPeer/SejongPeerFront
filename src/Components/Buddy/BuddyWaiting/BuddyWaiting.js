43b82759fd42cd4217f9a5aa31d6b338ea2306b3
Youjiiin <yujinjang24@gmail.com>
2024-03-03 01:05:40 +0900

    feat: 버디 매칭창, 매칭중, 상대찾음 통신

--- src/Components/Buddy/BuddyWaiting/BuddyWaiting.js
+++ src/Components/Buddy/BuddyWaiting/BuddyWaiting.js
@@ -7,6 +7,13 @@ const BuddyWaiting = ()=>{
     const moveToMain=()=>{
         navigate("/main");
     }
+    const buddyCancel = () => {
+      let comfirmcancel = confirm("정말 매칭을 취소하시겠습니까?");
+      if (comfirmcancel) {
+        cancelBuddy();
+      }
+    }
+
     const cancelBuddy = async() => {
         try {
             const response = await fetch(
@@ -15,7 +22,8 @@ const BuddyWaiting = ()=>{
                 method: "GET",
                 body: JSON.stringify(),
                 headers: {
-                  "Content-Type": "application/json",
+                  Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+                  'Refresh-Token': localStorage.getItem('refreshToken'),
                 },
               }
             );
@@ -27,7 +35,7 @@ const BuddyWaiting = ()=>{
               throw new Error(data.message);
             }
       
-            alert(data.message);
+            alert("버디 신청이 취소되었습니다.");
             console.log(data.message);
             navigate("/main");
           } catch (error) {
@@ -49,7 +57,7 @@ const BuddyWaiting = ()=>{
               <p className={style.text2}>버디를 찾으면 상대방의 정보(과, 학년) 확인 후 수락 / 거절할 수 있습니다!(최대 3회)</p>
               <p className={style.text3}>* 거절 시 한 시간 버디이용 제한 패널티가 부여됩니다.</p>
               <button className={style.moveToHomeBtn} onClick={moveToMain}>메인 페이지로 이동</button>
-              <button className={style.cancelBtn} onClick={cancelBuddy}>버디 신청 취소</button>
+              <button className={style.cancelBtn} onClick={buddyCancel}>버디 신청 취소</button>
             </div>
         </div>
     )
