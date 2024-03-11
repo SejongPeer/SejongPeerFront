12cc7de280d2ffadbf18473d5bf3844600aa34db
AhnRian0 <00rian@naver.com>
2024-03-12 00:31:00 +0900

    feat: 아이디/비번 찾기 로딩화면 추가

--- src/Components/Login/Find/FindId.js
+++ src/Components/Login/Find/FindId.js
@@ -5,12 +5,13 @@ import SignInBox from "../SignIn/SignInBox";
 import axios from "axios";
 import { MyContext } from "../../../App";
 import { useNavigate } from "react-router-dom";
-
+import Loading from "../../Loading/Loading";
 const FindId = () => {
   const navigate = useNavigate();
   const [id, setId] = useState("");
   const [pwd, setPWd] = useState("");
   const { setName, setStudentNum, studentNum, name } = useContext(MyContext);
+  const [loading, setLoading] = useState(false);
 
   const inputID = (idinput) => {
     setId(idinput);
@@ -19,49 +20,53 @@ const FindId = () => {
     setPWd(pwdinput);
   };
 
-  const isSejong = async() => {
+  const isSejong = async () => {
+
     try {
+      setLoading(true);
       // 세종어스 확인
       const response1 = await axios
-      .post(process.env.REACT_APP_BACK_SERVER + '/auth/sejong-auth', {
-        id: id,
-        pw: pwd,
-      })
-      .then((response) => {
-        let result = response.data.data.isAuth;
-        if (result === false)
-          alert("아이디 및 비밀번호가 일치하지 않습니다")
-        else if (result === true) {
-          alert("인증 완료");
-          setStudentNum(id);
-          setName(response.data.data.name);
-        }
-      })
+        .post(process.env.REACT_APP_BACK_SERVER + '/auth/sejong-auth', {
+          id: id,
+          pw: pwd,
+        })
+        .then((response) => {
+          let result = response.data.data.isAuth;
+          if (result === false)
+            alert("아이디 및 비밀번호가 일치하지 않습니다")
+          else if (result === true) {
+            alert("인증 완료");
+            setStudentNum(id);
+            setName(response.data.data.name);
+          }
+          setLoading(false);
+        })
 
       //아이디 찾기
       const response2 = await axios
-      .post(process.env.REACT_APP_BACK_SERVER + '/member/help/find-account',{
-        studentId : studentNum,
-        name : name,
-      });
+        .post(process.env.REACT_APP_BACK_SERVER + '/member/help/find-account', {
+          studentId: studentNum,
+          name: name,
+        });
 
       let findAccount = response2.data.data.account;
-      
+
       navigate("/login/resetpwd", { state: { findAccount, studentNum } });
 
-    } catch(err) {
+    } catch (err) {
       console.error(err);
-      if(err.response && err.response.status === 404){
-          console.log("404 에러 발생: ", err.response.status);
-          alert("해당 정보로 가입된 계정이 존재하지 않습니다.");
+      if (err.response && err.response.status === 404) {
+        console.log("404 에러 발생: ", err.response.status);
+        alert("해당 정보로 가입된 계정이 존재하지 않습니다.");
       }
-      else if(err.response && err.response.status === 400){
+      else if (err.response && err.response.status === 400) {
         console.log("400 에러 발생: ", err.response.status);
         alert("이름 또는 전화번호를 입력하지 않았습니다.");
       }
+      setLoading(false);
     }
   };
-  
+
   return (
     <div className={style.entire_Container}>
       <div className={style.container}>
@@ -71,16 +76,17 @@ const FindId = () => {
             세종대학교 포털로그인
           </p>
         </div>
-          <div className={style.aaa}>
-            <SignInBox inputID={inputID} name="포털 로그인 아이디(학번)" />
-            <SignInBox inputPwd={inputPwd} name="포털 로그인 비밀번호" id="pwd" />
-            <button className={style.signInBtn} onClick={isSejong}>
+        <div className={style.aaa}>
+          <SignInBox inputID={inputID} name="포털 로그인 아이디(학번)" />
+          <SignInBox inputPwd={inputPwd} name="포털 로그인 비밀번호" id="pwd" />
+          <button className={style.signInBtn} onClick={isSejong}>
             아이디/비밀번호 찾기
-            </button>
-            <a href="http://portal.sejong.ac.kr/" className={style.sejong}>
-              http://portal.sejong.ac.kr/
-            </a>
-          </div>
+          </button>
+          <a href="http://portal.sejong.ac.kr/" className={style.sejong}>
+            http://portal.sejong.ac.kr/
+          </a>
+        </div>
+        {loading ? <Loading /> : null}
       </div>
     </div>
   );
