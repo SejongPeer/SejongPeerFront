fa2eda08566df0d95e157062a076cb603b2d6762
Kimjuyoung <jjassb404@naver.com>
2024-05-12 00:03:53 +0900

    style : style-component 적용 시도

--- src/Pages/Festival/AnimalMatchResult/AnimalMatchInfo.js
+++ src/Pages/Festival/AnimalMatchResult/AnimalMatchInfo.js
@@ -1,22 +1,64 @@
 import {useState} from 'react';
 import { useNavigate, useContext } from 'react-router-dom'
 import style from './AnimalMatchInfo.module.css'
-// import SignInBox from './SignInBox';
 import { MyContext } from '../../../App';
+
+import styled from "styled-components";
 
 const AnimalMatchInfo = () => {
     console.log("AnimalMatchInfo 잘 들어옴!");
     return (
-        <div className={style.entire_Container}>
-            <h1>미팅정보입력</h1>
-          <div className={style.container}>
-            <div className={style.box} name="이름입력1">이름입력2</div>
-            <div className={style.box} name="전번입력1">전번입력2</div>
-            <button className={style.signInBtn}>
-              미팅시작  
-            </button>
-          </div>
-        </div>
-      );
+        <entire_Container>
+            <Container>
+                <Title>미팅정보입력</Title>
+                <Box>이름입력</Box>
+                <Box>전화번호입력</Box>
+                <MeetingStart>미팅시작</MeetingStart>
+          </Container>
+        </entire_Container>
+    );
 }
 export default AnimalMatchInfo;
+
+const entire_Container = styled.div`
+    width: 100vw;
+      height: 100vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+`
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 10vh;
+`
+const Title = styled.div`
+    font-size : 50px;
+`
+const MeetingStart = styled.div`
+    width: 335px;
+    height: 56px;
+    border-radius: 50px;
+    border: none;
+    background-color: #ff4b4b;
+    color: #ffffff;
+    font-size: 18px;
+    text-align: center;
+    margin: 12px 0px;
+    font-weight: bold;
+    cursor: pointer;
+`
+
+const Box = styled.div`
+    width: 335px;
+    height: 56px;
+    border-radius: 35px;
+    padding: 24px;
+    background-color: #FFFFFF;
+    border-color: #E5E5E5;
+    margin: 4px 0px;
+    font-size: 16px;
+    color: #999999;
+    border-style: solid;
+`
