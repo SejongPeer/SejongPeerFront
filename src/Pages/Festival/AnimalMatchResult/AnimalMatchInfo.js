2c190994f98f4508241b9eb7a630a60bd1c93645
Kimjuyoung <jjassb404@naver.com>
2024-05-11 23:36:45 +0900

    style : 미팅정보입력 CSS 1차

--- src/Pages/Festival/AnimalMatchResult/AnimalMatchInfo.js
+++ src/Pages/Festival/AnimalMatchResult/AnimalMatchInfo.js
@@ -1,12 +1,22 @@
 import {useState} from 'react';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useContext } from 'react-router-dom'
+import style from './AnimalMatchInfo.module.css'
+// import SignInBox from './SignInBox';
+import { MyContext } from '../../../App';
 
 const AnimalMatchInfo = () => {
     console.log("AnimalMatchInfo 잘 들어옴!");
-    return(
-    <div>
-         <h1>동물상 매칭 결과 확인 전 정보 입력 창입니다!</h1>
-    </div>
-    );
+    return (
+        <div className={style.entire_Container}>
+            <h1>미팅정보입력</h1>
+          <div className={style.container}>
+            <div className={style.box} name="이름입력1">이름입력2</div>
+            <div className={style.box} name="전번입력1">전번입력2</div>
+            <button className={style.signInBtn}>
+              미팅시작  
+            </button>
+          </div>
+        </div>
+      );
 }
 export default AnimalMatchInfo;
