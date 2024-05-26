7c23119e79d32561276d3fea662fac549abc8186
AhnRian0 <00rian@naver.com>
2024-05-26 16:21:42 +0900

    fix : QA사항 1차 수정

--- src/Pages/Honbob/Matching/JS/H_InformCheck.js
+++ src/Pages/Honbob/Matching/JS/H_InformCheck.js
@@ -8,31 +8,63 @@ const H_informCheck = props => {
       <p className={con.title}>입력하신 정보를 확인해주세요.</p>
       <div className={style.informContainer}>
         <div className={style.genderCon}>
-          <div>
-            <span style={{ color: '#FF7474', fontSize: '2.5vh' }}>•</span>{' '}
-            대동지 성별
+          <div className={style.titleContainer}>
+            <span
+              style={{
+                color: '#FF7474',
+                fontSize: '2.5vh',
+                fontWeight: 'bold',
+              }}
+            >
+              •
+            </span>{' '}
+            <span className={style.font}>대동지 성별</span>
           </div>
 
           <div className={style.divBox}>{props.choiceGenderKorean}</div>
         </div>
         <div className={style.menuCon}>
-          <div>
-            <span style={{ color: '#FF7474', fontSize: '2.5vh' }}>•</span> 선호
-            액티비티
+          <div className={style.titleContainer}>
+            <span
+              style={{
+                color: '#FF7474',
+                fontSize: '2.5vh',
+                fontWeight: 'bold',
+              }}
+            >
+              •
+            </span>{' '}
+            <span className={style.font}>선호 액티비티</span>
           </div>
           <div className={style.divBox}>{props.choiceMenuKorean}</div>
         </div>
         <div className={style.kakaoCon}>
-          <div>
-            <span style={{ color: '#FF7474', fontSize: '2.5vh' }}>•</span>{' '}
-            카카오톡 아이디
+          <div className={style.titleContainer}>
+            <span
+              style={{
+                color: '#FF7474',
+                fontSize: '2.5vh',
+                fontWeight: 'bold',
+              }}
+            >
+              •
+            </span>{' '}
+            <span className={style.font}>카카오톡 아이디</span>
           </div>
           <div className={style.divBox}>{kakaoAccount}</div>
         </div>
         <div className={style.phoneNumCon}>
-          <div>
-            <span style={{ color: '#FF7474', fontSize: '2.5vh' }}>•</span>{' '}
-            전화번호
+          <div className={style.titleContainer}>
+            <span
+              style={{
+                color: '#FF7474',
+                fontSize: '2.5vh',
+                fontWeight: 'bold',
+              }}
+            >
+              •
+            </span>{' '}
+            <span className={style.font}>전화번호</span>
           </div>
           <div className={style.divBox}>{phoneNumber}</div>
         </div>
